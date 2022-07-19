4c0206b33565273e7d4e570158d72a7fcfbb1588
Toan Do <toan.do@doctoranywhere.com>
2022-07-19 09:36:38 +0700

    Adapted code style

--- src/main/java/io/awesome/ui/components/dialog/ConfirmDialog.java
+++ src/main/java/io/awesome/ui/components/dialog/ConfirmDialog.java
@@ -16,110 +16,120 @@ import java.util.List;
 
 public class ConfirmDialog extends Dialog {
 
-    private String title = "";
-    private String message = "";
-    private HorizontalLayout header;
-    private VerticalLayout body;
-    private HorizontalLayout footer;
-    private Button confirmButton;
+  private String title = "";
+  private String message = "";
+  private HorizontalLayout header;
+  private VerticalLayout body;
+  private HorizontalLayout footer;
+  private Button confirmButton;
 
-    public ConfirmDialog(String... classNames) {
-        VerticalLayout dialogLayout = createDialogLayout(classNames);
-        this.add(dialogLayout);
+  public ConfirmDialog(String... classNames) {
+    VerticalLayout dialogLayout = createDialogLayout(classNames);
+    this.add(dialogLayout);
+  }
+
+  public ConfirmDialog(String title, String message, String... classNames) {
+    this(classNames);
+    this.setTitle(title);
+    this.setMessage(message);
+  }
+
+  private VerticalLayout createDialogLayout(String... classNames) {
+    this.header = buildHeaderLayout();
+    this.body = buildBodyLayout();
+    this.footer = buildFooterLayout(classNames);
+
+    VerticalLayout dialogLayout = new VerticalLayout(header, body, footer);
+    dialogLayout.setPadding(false);
+    dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+    dialogLayout.getStyle().set("width", "600px").set("max-width", "100%");
+
+    return dialogLayout;
+  }
+
+  private HorizontalLayout buildHeaderLayout() {
+    H2 title = new H2(this.title);
+    title.setClassName("title");
+    title.getElement().setAttribute("title", this.title);
+    title
+        .getStyle()
+        .set("margin", "-5px 0 0 15px")
+        .set("font-size", "22px")
+        .set("font-weight", "bold");
+    HorizontalLayout headerLayout = new HorizontalLayout(title);
+    headerLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.START);
+    return headerLayout;
+  }
+
+  private HorizontalLayout buildFooterLayout(String... classNames) {
+    confirmButton = new Button("Confirm");
+    List<String> classButton = new ArrayList<>(List.of("control-button"));
+    if (classNames != null && classNames.length > 0) {
+      Arrays.stream(classNames).forEach(classButton::add);
+    } else {
+      classButton.add("green-button");
     }
+    confirmButton.addClassNames(classButton.toArray(new String[0]));
+    Button cancelButton = new Button("Cancel", e -> this.close());
+    cancelButton.addClassNames("control-button", "blue-button");
 
-    public ConfirmDialog(String title, String message, String... classNames) {
-        this(classNames);
-        this.setTitle(title);
-        this.setMessage(message);
-    }
+    HorizontalLayout footerLayout = new HorizontalLayout(confirmButton, cancelButton);
+    footerLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+    footerLayout.getStyle().set("margin", "20px 0 -15px 0");
+    return footerLayout;
+  }
 
-    private VerticalLayout createDialogLayout(String... classNames) {
-        this.header = buildHeaderLayout();
-        this.body = buildBodyLayout();
-        this.footer = buildFooterLayout(classNames);
+  private VerticalLayout buildBodyLayout() {
+    Span messageField = new Span(this.message);
+    messageField.getElement().setAttribute("message", message);
+    VerticalLayout bodyLayout = new VerticalLayout(messageField);
+    bodyLayout.setSpacing(false);
+    bodyLayout.setPadding(false);
+    bodyLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+    return bodyLayout;
+  }
 
-        VerticalLayout dialogLayout = new VerticalLayout(header, body,
-                footer);
-        dialogLayout.setPadding(false);
-        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        dialogLayout.getStyle().set("width", "600px").set("max-width", "100%");
+  public void setConfirmListener(ComponentEventListener<ClickEvent<Button>> confirmListener) {
+    this.confirmButton.addClickListener(confirmListener);
+  }
 
-        return dialogLayout;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+    this.header
+        .getElement()
+        .getChildren()
+        .filter(element -> element.hasAttribute("title"))
+        .findFirst()
+        .ifPresent(
+            element -> {
+              element.setText(title);
+              element.setAttribute("title", title);
+            });
+  }
 
-    private HorizontalLayout buildHeaderLayout() {
-        H2 title = new H2(this.title);
-        title.setClassName("title");
-        title.getElement().setAttribute("title", this.title);
-        title.getStyle().set("margin", "-5px 0 0 15px")
-                .set("font-size", "22px").set("font-weight", "bold");
-        HorizontalLayout headerLayout = new HorizontalLayout(title);
-        headerLayout
-                .setJustifyContentMode(FlexComponent.JustifyContentMode.START);
-        return headerLayout;
-    }
+  public void setMessage(String message) {
+    this.message = message;
+    this.body
+        .getElement()
+        .getChildren()
+        .filter(element -> element.hasAttribute("message"))
+        .findFirst()
+        .ifPresent(
+            element -> {
+              element.setText(message);
+              element.setAttribute("message", message);
+            });
+  }
 
-    private HorizontalLayout buildFooterLayout(String... classNames) {
-        confirmButton = new Button("Confirm");
-        List<String> classButton = new ArrayList<>(List.of("control-button"));
-        if (classNames != null && classNames.length > 0) {
-            Arrays.stream(classNames).forEach(classButton::add);
-        } else {
-            classButton.add("green-button");
-        }
-        confirmButton.addClassNames(classButton.toArray(new String[0]));
-        Button cancelButton = new Button("Cancel", e -> this.close());
-        cancelButton.addClassNames("control-button", "blue-button");
+  public HorizontalLayout getHeader() {
+    return header;
+  }
 
-        HorizontalLayout footerLayout = new HorizontalLayout(confirmButton, cancelButton);
-        footerLayout
-                .setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-        footerLayout.getStyle().set("margin", "20px 0 -15px 0");
-        return footerLayout;
-    }
+  public VerticalLayout getBody() {
+    return body;
+  }
 
-    private VerticalLayout buildBodyLayout() {
-        Span messageField = new Span(this.message);
-        messageField.getElement().setAttribute("message", message);
-        VerticalLayout bodyLayout = new VerticalLayout(messageField);
-        bodyLayout.setSpacing(false);
-        bodyLayout.setPadding(false);
-        bodyLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        return bodyLayout;
-    }
-
-    public void setConfirmListener(ComponentEventListener<ClickEvent<Button>> confirmListener) {
-        this.confirmButton.addClickListener(confirmListener);
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-        this.header.getElement().getChildren().filter(element -> element.hasAttribute("title"))
-                .findFirst().ifPresent(element -> {
-                    element.setText(title);
-                    element.setAttribute("title", title);
-                });
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-        this.body.getElement().getChildren().filter(element -> element.hasAttribute("message"))
-                .findFirst().ifPresent(element -> {
-                    element.setText(message);
-                    element.setAttribute("message", message);
-                });
-    }
-
-    public HorizontalLayout getHeader() {
-        return header;
-    }
-
-    public VerticalLayout getBody() {
-        return body;
-    }
-
-    public HorizontalLayout getFooter() {
-        return footer;
-    }
+  public HorizontalLayout getFooter() {
+    return footer;
+  }
 }
--- src/main/java/io/awesome/ui/errors/CustomErrorHandler.java
+++ src/main/java/io/awesome/ui/errors/CustomErrorHandler.java
@@ -4,35 +4,31 @@ import com.google.common.base.Throwables;
 import com.vaadin.flow.server.ErrorEvent;
 import com.vaadin.flow.server.ErrorHandler;
 import io.awesome.util.NotificationUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class CustomErrorHandler implements ErrorHandler {
-    private static final Logger log = LoggerFactory.getLogger(CustomErrorHandler.class);
-    @Override
-    public void error(ErrorEvent errorEvent) {
-        log.error("Uncaught UI exception", errorEvent.getThrowable());
-        NotificationUtil.error(
-                "We are sorry, but an internal error occurred: "
-                        + getMessage(errorEvent.getThrowable()));
-    }
+  private static final Logger log = LoggerFactory.getLogger(CustomErrorHandler.class);
 
-    private String getMessage(Throwable throwable) {
-        if (throwable.getCause() != null) {
-            Throwable nestedError = throwable.getCause();
-            if (nestedError instanceof DataAccessException) {
-                String rootMessage = Throwables.getRootCause(nestedError).getMessage();
-                if (rootMessage.contains("Detail: ")) {
-                    return rootMessage.substring(rootMessage.indexOf("Detail: "));
-                }
-                return rootMessage;
-            }
+  @Override
+  public void error(ErrorEvent errorEvent) {
+    log.error("Uncaught UI exception", errorEvent.getThrowable());
+    NotificationUtil.error(
+        "We are sorry, but an internal error occurred: " + getMessage(errorEvent.getThrowable()));
+  }
+
+  private String getMessage(Throwable throwable) {
+    if (throwable.getCause() != null) {
+      Throwable nestedError = throwable.getCause();
+      if (nestedError instanceof DataAccessException) {
+        String rootMessage = Throwables.getRootCause(nestedError).getMessage();
+        if (rootMessage.contains("Detail: ")) {
+          return rootMessage.substring(rootMessage.indexOf("Detail: "));
         }
-        return throwable.getMessage();
+        return rootMessage;
+      }
     }
+    return throwable.getMessage();
+  }
 }
